fe2345aa368220e93e118c67f9cb779929304c4e
Layken Varholdt <laykenv@gmail.com>
2025-10-22 11:54:14 -0500

    remove sync func

--- app/portal/subscribe/page.tsx
+++ app/portal/subscribe/page.tsx
@@ -1,12 +1,11 @@
 "use client";
-import { useAction, useQuery } from "convex/react";
+import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { CheckoutLink } from "@convex-dev/polar/react";
 
 export default function SubscribePage() {
   const products = useQuery(api.polarSettings.getConfiguredProducts);
   const singleProduct = products?.subscription as { id: string };
-  const syncProducts = useAction(api.agreement.syncProducts);
 
   if (!products) {
     return <div>Loading...</div>;
@@ -14,8 +13,9 @@ export default function SubscribePage() {
 
   return <div>
     <h1>Subscribe</h1>
-    <button onClick={() => syncProducts()}>Sync Products</button>
     <div>{JSON.stringify(products)}</div>
-    <CheckoutLink polarApi={api.polarSettings} productIds={[singleProduct.id]}></CheckoutLink>
+    <CheckoutLink polarApi={api.polarSettings} productIds={[singleProduct.id]}>
+      <button>Upgrade to Subscription</button>
+    </CheckoutLink>
   </div>;
 }
