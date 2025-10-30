6528e9d16b6ea731ebd8cfe0781cf415139780b6
Layken Varholdt <laykenv@gmail.com>
2025-10-29 21:18:40 -0500

    updated cal urls

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,6 @@
 export const ONBOARDING_CAL_LINK = "https://cal.com/layken-varholdt/agency-prospect";
-export const CAL_KICKOFF_URL = "https://cal.com/layken-varholdt/kickoff";
-export const CAL_REVIEW_URL = "https://cal.com/layken-varholdt/review";
+export const CAL_KICKOFF_URL = "https://cal.com/layken-varholdt/website-kickoff-call";
+export const CAL_REVIEW_URL = "https://cal.com/layken-varholdt/website-review-call";
 
 export const SITE_URL = process.env.SITE_URL ?? "http://localhost:3000";
 
